d1dd4f1b7e2180cb777ed6f377201c8bfc637e5b
clcoder2425 <141270027+clcoder2425@users.noreply.github.com>
2024-01-29 13:39:03 -0600

    added server data

---
+++ server.js
@@ -0,0 +1,19 @@
+// Importing express
+
+const express = require('express');
+
+const dataBase = require('./config/connection');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+dataBase.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Social Network API runing on port ${PORT}`);
+    });
+
+});
