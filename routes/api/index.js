ee309d83e966973ddc40f674850feac091667460
clcoder2425 <141270027+clcoder2425@users.noreply.github.com>
2024-02-02 12:03:49 -0600

    added comments to api/index file

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 const user_routes = require('./userRoutes');
 const thought_routes = require('./thoughtRoutes');
 
-//Specifying endpoint for user and thought routes
+//Endpoint for user and thought routes
 
 router.use('/user', user_routes);
 router.use('/thought', thought_routes);
